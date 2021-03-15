0854251bf8acce0f8cd52382f55f11bcf12606fa
vpispanen <vpispanen@gmail.com>
2021-03-15 20:46:25 +0300

    initial commit 2

--- src/com/company/DivisionMain.java
+++ src/com/company/DivisionMain.java
@@ -3,7 +3,7 @@ package com.company;
 public class DivisionMain {
     public static void main(String[] args) {
         int x = 123;
-        int result1 = x / 10;
+        int result1 = x / 100;
         int result2 = (int)(x * 0.1);
         System.out.println(result1);
         System.out.println(result2);
